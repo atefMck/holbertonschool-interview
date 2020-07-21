1558015bb5a3baf1ddc42de55e79e6cb52080859
atefsama666 <mechken.atef@gmail.com>
2020-07-21 18:43:55 +0100

    Pep8 fixed

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,7 +5,7 @@ def canUnlockAll(boxes):
     can_unlock = [False for i in boxes]
     can_unlock[0] = True
     for i in range(len(boxes)):
-        if can_unlock[i] == False:
+        if can_unlock[i] is False:
             if i in keys:
                 can_unlock[i] = True
                 keys += [k for k in boxes[i]]
