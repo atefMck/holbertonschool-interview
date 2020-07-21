6f989409568a7462476a05a8f5b36cb250acf3d4
atefsama666 <mechken.atef@gmail.com>
2020-07-21 23:49:33 +0100

    Handled exceptions

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,17 +4,20 @@ def canUnlockAll(boxes):
     keys = [i for i in boxes[0]]
     can_unlock = [False for i in boxes]
     can_unlock[0] = True
-    for i in range(len(boxes)):
-        if can_unlock[i] is False:
-            if i in keys:
-                can_unlock[i] = True
-                for k in boxes[i]:
-                    if k not in keys:
-                        keys += [k]
-                for j in boxes[i]:
-                    for k in boxes[j]:
+    try:
+        for i in range(len(boxes)):
+            if can_unlock[i] is False:
+                if i in keys:
+                    can_unlock[i] = True
+                    for k in boxes[i]:
                         if k not in keys:
                             keys += [k]
-            else:
-                can_unlock[i] = False
+                    for j in boxes[i]:
+                        for k in boxes[j]:
+                            if k not in keys:
+                                keys += [k]
+                else:
+                    can_unlock[i] = False
+    except:
+        pass
     return all(can_unlock)
