55c3b73b8b194915032807f80b751db2830efb19
atefsama666 <mechken.atef@gmail.com>
2020-07-21 23:42:37 +0100

    Added type check and length check

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
     """check if all boxes can be opened"""
+    if type(boxes) != list:
+        return False
+    if len(boxes) == 0:
+        return False
     keys = [i for i in boxes[0]]
     can_unlock = [False for i in boxes]
     can_unlock[0] = True
@@ -8,9 +12,15 @@ def canUnlockAll(boxes):
         if can_unlock[i] is False:
             if i in keys:
                 can_unlock[i] = True
-                keys += [k for k in boxes[i]]
+                for k in boxes[i]:
+                    if k not in keys:
+                        keys += [k]
                 for j in boxes[i]:
-                    keys += [k for k in boxes[j]]
+                    for k in boxes[j]:
+                        if k not in keys:
+                            keys += [k]
             else:
                 can_unlock[i] = False
+    print(keys)
+    print(can_unlock)
     return all(can_unlock)
