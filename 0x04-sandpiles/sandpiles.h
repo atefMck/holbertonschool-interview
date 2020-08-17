cc859905dd5f1f97d172f33992ec12417e7d4745
atefsama666 <mechken.atef@gmail.com>
2020-08-17 13:46:24 +0100

    Task 0

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,9 @@
+#ifndef _SANDPILES_
+#define _SANDPILES_
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void grid_sum(int grid1[3][3], int grid2[3][3]);
+void topple(int grid[3][3]);
+int is_stable(int grid[3][3]);
+
+#endif
