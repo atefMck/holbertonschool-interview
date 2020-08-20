4cea43624334779fbefaec95fbfa03a8d954b6fc
atefsama666 <mechken.atef@gmail.com>
2020-08-20 12:55:14 +0100

    Task 0 attempt 1

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Minimum operations module
+"""
+
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations 
+    needed to result n characters
+    """
+    op_count = 0
+    while n != 1:
+        if n % 2 == 1:
+            n = n - 1
+            op_count = op_count + 1
+        else:
+            n = n // 2
+            op_count = op_count + 2
+        if n < 1:
+            return 0
+    return op_count
