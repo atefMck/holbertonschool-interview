fdec0bd9824f30fd82e1c8be28f52b6f01073e79
atefsama666 <mechken.atef@gmail.com>
2020-08-20 13:50:11 +0100

    Whole algorithm overhaul

--- 0x03-minimum_operations/0-main.py
+++ 0x03-minimum_operations/0-main.py
@@ -10,3 +10,7 @@ print("Min number of operations to reach {} characters: {}".format(n, minOperati
 
 n = 12
 print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+
+n = 19170307
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -9,17 +9,8 @@ def minOperations(n):
     Calculates the fewest number of operations needed to result n characters
     """
     op_count = 0
-    num_char = 1
-    if not isinstance(n, int):
-        return 0
-    if n <= 0:
-        return 0
-    tmp = n
-    while tmp != 1:
-        if tmp % 2 == 1:
-            tmp = tmp - 1
-            op_count = op_count + 1
-        else:
-            tmp = tmp // 2
-            op_count = op_count + 2
+    for i in range(2, n):
+        while n % i == 0:
+            op_count += i
+            n = n // i
     return op_count
