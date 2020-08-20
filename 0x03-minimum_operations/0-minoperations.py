9ee8a97ecb7df8dcd23a231e2e649dd389cb8890
atefsama666 <mechken.atef@gmail.com>
2020-08-20 13:09:07 +0100

    Added conditions to handle n param

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -9,6 +9,11 @@ def minOperations(n):
     Calculates the fewest number of operations needed to result n characters
     """
     op_count = 0
+    num_char = 1
+    if not isinstance(n, int):
+        return 0
+    if n <= 0:
+        return 0
     while n != 1:
         if n % 2 == 1:
             n = n - 1
@@ -16,6 +21,4 @@ def minOperations(n):
         else:
             n = n // 2
             op_count = op_count + 2
-        if n < 1:
-            return 0
     return op_count
