8d335ccbf53a3e8c81df5a68600b7ecc6a3fe332
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2020-10-08 18:29:15 +0100

    Unsaved changes. (?)

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+ int is_palindrome(listint_t **head) {
+     
+ }
