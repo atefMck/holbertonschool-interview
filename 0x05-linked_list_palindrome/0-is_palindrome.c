eb33bb30f24a893fcc48f21a219efc92763a33d7
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2020-10-09 16:59:15 +0100

    Pushing full program

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,12 +1,36 @@
+#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "lists.h"
-
+#include <stddef.h>
 /**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
- */
- int is_palindrome(listint_t **head) {
-     
- }
+* is_palindrome - checks if a singly list is palindrome
+* @head: double pointer to the head
+* Return: 1 if palindrome, 0 otherwise
+*/
+int is_palindrome(listint_t **head)
+{
+int j = 0, i = 0, s;
+listint_t *beg, *end;
+if (!head)
+return (0);
+if (!(*head))
+return (1);
+end = *head;
+beg = *head;
+j += 1;
+while (end->next)
+{
+end = end->next;
+j++;
+}
+s = j / 2;
+while (i <= s)
+{
+if (beg->n != end->n)
+return (0);
+beg += 2;
+end -= 2;
+i++;
+}
+return (1);
+}
