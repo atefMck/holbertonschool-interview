1bc9d3ef018ce8cfbf757fee2a7fff9f7d097c91
atefsama666 <mechken.atef@gmail.com>
2020-12-09 12:27:59 +0000

    Fixes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,13 +3,12 @@ def canUnlockAll(boxes):
     """check if all boxes can be opened"""
     keys = [0] + [i for i in boxes[0]]
     needed_keys = [i for i in range(len(boxes))]
-    for i in range(len(boxes)):
-        temp = keys
-        for j in temp:
-            if j < len(boxes):
-                for k in boxes[j]:
-                    if k not in keys:
-                        keys.append(k)
+    for j in keys:
+        if j < len(boxes):
+            for k in boxes[j]:
+                print(k)
+                if k not in keys:
+                    keys.append(k)
     for k in needed_keys:
         if k not in keys:
             return False
