50092b5f801a5cc47f6eb88b024785e1ef1d46c4
atefsama666 <mechken.atef@gmail.com>
2020-12-09 11:43:15 +0000

    New try

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
     """check if all boxes can be opened"""
-    try:
-        keys = [0] + [i for i in boxes[0]]
-    except:
-        return False
-    can_unlock = [False for i in boxes]
-    try:
-        for i in range(len(boxes)):
-            if can_unlock[i] is False or i == 0:
-                if i in keys:
-                    can_unlock[i] = True
-                    for k in boxes[i]:
-                        if k not in keys:
-                            keys += [k]
-                    for j in boxes[i]:
-                        for k in boxes[j]:
-                            if k not in keys:
-                                keys += [k]
-                else:
-                    can_unlock[i] = False
-    except:
-        pass
-    return all(can_unlock)
+    keys = [0] + [i for i in boxes[0]]
+    needed_keys = [i for i in range(len(boxes))]
+    for i in range(len(boxes)):
+        temp = keys
+        for j in temp:
+            if j < len(boxes):
+                for k in boxes[j]:
+                    if k not in keys:
+                        keys.append(k) 
+    for k in range(len(boxes)):
+        if k not in keys:
+            return False
+    return True
+
