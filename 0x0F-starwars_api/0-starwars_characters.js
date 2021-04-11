d2d959eccb5b1a16d9c0a860034960352c5d76dd
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-11 15:36:14 +0100

    Task 0 fixed

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,13 +1,24 @@
 #!/usr/bin/node
+const { rejects } = require('assert');
 const request = require('request');
 
 const movieId = process.argv[2];
 
-request(`https://swapi-api.hbtn.io/api/films/${movieId}/`, (_, _0, body) => {
-  const characters = JSON.parse(body).characters;
-  characters.forEach(char => {
-    request(char, (_, _0, body) => {
-      console.log(JSON.parse(body).name);
+const displayNamesAsync = async char => {
+  return new Promise((resolve, reject) => {
+    request(char, (error, _0, body) => {
+      if (body) {
+        resolve(JSON.parse(body).name);
+      } else {
+        reject(error);
+      }
     });
   });
+};
+
+request(`https://swapi-api.hbtn.io/api/films/${movieId}/`, async (_, _0, body) => {
+  const characters = JSON.parse(body).characters;
+  for (const char of characters) {
+    console.log(await displayNamesAsync(char));
+  }
 });
