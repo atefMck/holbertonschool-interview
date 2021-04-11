58fa5f24e468b83b26f6a7c4ed88a39d75fa5042
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-11 15:46:13 +0100

    added error handling

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -5,22 +5,23 @@ const movieId = process.argv[2];
 
 const displayNamesAsync = async char => {
   return new Promise((resolve, reject) => {
-    request(char, (error, _0, body) => {
-      if (body) {
+    request(char, (error, response, body) => {
+      if (response) {
         resolve(JSON.parse(body).name);
-      } else {
+      } else if (error) {
         reject(error);
       }
     });
   });
 };
 
-request(`https://swapi-api.hbtn.io/api/films/${movieId}/`, async (_, _0, body) => {
-  const characters = JSON.parse(body).characters;
-  for (const char of characters) {
-    const charName = await displayNamesAsync(char);
-    if (charName) {
-      console.log(charName);
+request(`https://swapi-api.hbtn.io/api/films/${movieId}/`, async (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else if (response) {
+    const characters = JSON.parse(body).characters;
+    for (const char of characters) {
+      console.log(await displayNamesAsync(char));
     }
   }
 });
