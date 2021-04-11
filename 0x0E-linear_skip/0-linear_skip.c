566a07577f575e6a0ffbba8475623f53f82f9c03
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-11 14:27:02 +0100

    betty styled

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -12,35 +12,38 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    skiplist_t *express, *tmp;
-    express = tmp = list;
+	skiplist_t *express, *tmp;
 
-    if (!list)
-    return (NULL);
-    while (express->express)
-    {
-        tmp = express;
-        express = express->express;
-        printf("Value checked at index [%ld] = [%d]\n", express->index, express->n);
-        if (express->n >= value || !express->express) {
-            if (!express->express && express->n < value)
-            {
-                tmp = express;
-                while (express->next)
-                express = express->next;
-            }
-            printf("Value found between indexes [%ld] and [%ld]\n", tmp->index, express->index);
-            while (tmp != express)
-            {
-                printf("Value checked at index [%ld] = [%d]\n", tmp->index, tmp->n);
-                if (tmp->n == value)
-                return (tmp);
-                tmp = tmp->next;
-            }
-            if (express->n < value)
-            printf("Value checked at index [%ld] = [%d]\n", tmp->index, tmp->n);
-            return (NULL);
-        }
-    }
-    return (NULL);
+	express = tmp = list;
+
+	if (!list)
+	return (NULL);
+	while (express->express)
+	{
+		tmp = express;
+		express = express->express;
+		printf("Value checked at index [%ld] = [%d]\n", express->index, express->n);
+		if (express->n >= value || !express->express)
+		{
+			if (!express->express && express->n < value)
+			{
+				tmp = express;
+				while (express->next)
+				express = express->next;
+			}
+			printf("Value found between indexes [%ld] and [%ld]\n", tmp->index,
+			 express->index);
+			while (tmp != express)
+			{
+				printf("Value checked at index [%ld] = [%d]\n", tmp->index, tmp->n);
+				if (tmp->n == value)
+				return (tmp);
+				tmp = tmp->next;
+			}
+			if (express->n < value)
+			printf("Value checked at index [%ld] = [%d]\n", tmp->index, tmp->n);
+			return (NULL);
+		}
+	}
+	return (NULL);
 }
