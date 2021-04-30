3f501cdc9f17fb3c9ad17f3a88ff6b6b63bfe7fc
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-30 14:19:51 +0100

    Task dump because docker

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+#define swap(x, y) { int temp = x; x = y; y = temp; }
+
+
+/**
+* heapify - heapifies an array
+* @array: the array to sort
+* @n: array size
+* @idx: index of the root
+* Return: 1 if n is palindrome, 0 otherwise
+*/
+void heapify(int *array, size_t size, size_t calcsize, int idx)
+{
+    int root = idx;
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
+    int n = (int) size;
+
+    if (left < n && array[left] > array[root])
+    root = left;
+    if (right < n && array[right] > array[root])
+    root = right;
+    if (root != idx)
+    {
+        swap(array[idx], array[root]);
+        print_array(array, calcsize);
+        heapify(array, n, calcsize, root);
+    }
+}
+
+/**
+* heap_sort - function that sorts an array using heap sort
+* @array: the array to sort
+* @n: array size
+* Return: 1 if n is palindrome, 0 otherwise
+*/
+void heap_sort(int *array, size_t size)
+{
+    int i;
+    for (i = size / 2 - 1; i >= 0; i--)
+    heapify(array, size, size, i);
+    for (i = size - 1; i > 0; i--)
+    {
+        swap(array[0], array[i]);
+        print_array(array, size);
+        heapify(array, i, size, 0);
+    }
+}
