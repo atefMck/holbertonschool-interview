1e8cc5ce88f95f9c648c951f16f021b66b19d9b8
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-08-03 16:18:05 +0100

    Fixes

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -13,35 +13,35 @@
  */
 List *add_node_end(List **list, char *str)
 {
-    List *newNode, *head, *tail;
+	List *newNode, *head, *tail;
 
-    if (!str)
-    return (NULL);
+	if (!str)
+	return (NULL);
 
 
-    newNode = malloc(sizeof(List));
-    if (!newNode)
-    return (NULL);
-    newNode->str = strdup(str);
-    if (!newNode->str)
-    return (NULL);
+	newNode = malloc(sizeof(List));
+	if (!newNode)
+	return (NULL);
+	newNode->str = strdup(str);
+	if (!newNode->str)
+	return (NULL);
 
-    if (!list || *list == NULL)
-    {
-        newNode->next = newNode;
-        newNode->prev = newNode;
-        *list = newNode;
-        return (newNode);
-    }
+	if (!list || *list == NULL)
+	{
+		newNode->next = newNode;
+		newNode->prev = newNode;
+		*list = newNode;
+		return (newNode);
+	}
 
-    head = *list;
-    tail = head->prev;
-    newNode->next = head;
-    newNode->prev = tail;
-    tail->next = newNode;
-    head->prev = newNode;
+	head = *list;
+	tail = head->prev;
+	newNode->next = head;
+	newNode->prev = tail;
+	tail->next = newNode;
+	head->prev = newNode;
 
-    return (newNode);
+	return (newNode);
 
 }
 
@@ -55,34 +55,34 @@ List *add_node_end(List **list, char *str)
  */
 List *add_node_begin(List **list, char *str)
 {
-    List *newNode, *head, *tail;
+	List *newNode, *head, *tail;
 
-    if (!str)
-    return (NULL);
+	if (!str)
+	return (NULL);
 
 
-    newNode = malloc(sizeof(List));
-    if (!newNode)
-    return (NULL);
-    newNode->str = strdup(str);
-    if (!newNode->str)
-    return (NULL);
+	newNode = malloc(sizeof(List));
+	if (!newNode)
+	return (NULL);
+	newNode->str = strdup(str);
+	if (!newNode->str)
+	return (NULL);
 
-    if (!list || !*list)
-    {
-        newNode->next = newNode;
-        newNode->prev = newNode;
-        *list = newNode;
-        return (newNode);
-    }
+	if (!list || !*list)
+	{
+		newNode->next = newNode;
+		newNode->prev = newNode;
+		*list = newNode;
+		return (newNode);
+	}
 
-    head = *list;
-    tail = head->prev;
-    newNode->next = head;
-    newNode->prev = tail;
-    tail->next = newNode;
-    head->prev = newNode;
-    *list = newNode;
+	head = *list;
+	tail = head->prev;
+	newNode->next = head;
+	newNode->prev = tail;
+	tail->next = newNode;
+	head->prev = newNode;
+	*list = newNode;
 
-    return (newNode);
+	return (newNode);
 }
