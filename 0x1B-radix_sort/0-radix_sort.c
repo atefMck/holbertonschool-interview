5bd374ba34885207337ab5a22449cd573bbd9998
atefMCK <mechken.atef@gmail.com>
2021-08-04 13:13:20 +0000

    Betty styled

--- 0x1B-radix_sort/0-radix_sort.c
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -3,67 +3,71 @@
 #include "sort.h"
 
 /**
- * radix_sort - Radix sort an array
+ * getmax - Get maximum of array
  * @array: The array to sort
  * @size: Size of the array
  * Return: Always 0
  */
 int getmax(int *array, size_t size)
 {
-    size_t i;
-    int max = array[0];
-    for (i = 0; i < size; i++)
-    {
-        if (array[i] > max)
+	size_t i;
+	int max = array[0];
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > max)
 		max = array[i];
-    }
-    return max;
+	}
+	return (max);
 }
 
 /**
  * counting_sort - counting sort an array
  * @array: The array to sort
  * @size: Size of the array
+ * @sig: Significant digit
  * Return: Always 0
  */
 void counting_sort(int *array, size_t size, size_t sig)
 {
-    
-    int base[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    size_t i;
-    int j;
-    int *digit = malloc(sizeof(int) * size);
-    if (!digit)
-    return;
-    int *copy = malloc(sizeof(int) * size); 
-    if (!copy)
-    return;
+	int base[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	size_t i;
+	int j;
+	int *digit = malloc(sizeof(int) * size);
+	int *copy = malloc(sizeof(int) * size);
 
-    for (i = 0; i < size; i++)
-    copy[i] = array[i];
+	if (!digit)
+	return;
+	if (!copy)
+	return;
 
-    for (i = 0; i < size; i++)
-    {
-        digit[i] = (array[i] / sig) % 10;
-    }
+	for (i = 0; i < size; i++)
+	copy[i] = array[i];
 
-    for (i = 0; i < size; i++)
-    base[digit[i]]++;
+	for (i = 0; i < size; i++)
+	{
+		digit[i] = (array[i] / sig) % 10;
+	}
 
-    for (i = 0; i < 10; i++) {
-        if (i != 0) {
-            base[i] += base[i - 1];
-        }
-    }
+	for (i = 0; i < size; i++)
+	base[digit[i]]++;
 
-    for (j = (int) size; j >= 0; j--)
-    {
-        array[base[digit[j]] - 1] = copy[j];
-        base[digit[j]]--;
-    }
-    free(digit);
-    free(copy);
-    
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 0)
+		{
+			base[i] += base[i - 1];
+		}
+	}
+
+	for (j = (int) size; j >= 0; j--)
+	{
+		array[base[digit[j]] - 1] = copy[j];
+		base[digit[j]]--;
+	}
+	free(digit);
+	free(copy);
+
 }
 
 /**
@@ -74,13 +78,14 @@ void counting_sort(int *array, size_t size, size_t sig)
  */
 void radix_sort(int *array, size_t size)
 {
-    int max, i;
-    if (!array || size < 2)
-    return;
-    max = getmax(array, size);
-    for (i = 1; max / i != 0; i *= 10)
-    {
-        counting_sort(array, size, i);
-        print_array(array, size);
-    }
+	int max, i;
+
+	if (!array || size < 2)
+	return;
+	max = getmax(array, size);
+	for (i = 1; max / i != 0; i *= 10)
+	{
+		counting_sort(array, size, i);
+		print_array(array, size);
+	}
 }
