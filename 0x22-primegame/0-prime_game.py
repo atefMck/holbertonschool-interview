65ff566c8bad3302c0645152b6728a661dca1782
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-09-19 13:16:37 +0000

    Task 0

---
+++ 0x22-primegame/0-prime_game.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+def isPrime(num):
+  if num > 1:
+    for i in range(2, int(num/2)+1):
+      if (num % i) == 0:
+        return False
+    return True
+  else:
+      return False
+
+def isWinner(x, nums):
+  roundsWon = {
+    "Maria": 0,
+    "Ben": 0
+  }
+  turn = 0
+  for i in range(x):
+    choices = [i for i in range(1, nums[i] + 1)]
+    for j in choices:
+      if (isPrime(j)):
+        for k in choices:
+          if k % j == 0:
+            choices.remove(k)
+        turn = turn + 1 % 2
+    if turn == 0:
+      roundsWon["Maria"] += 1
+      turn = 1
+    else:
+      roundsWon["Ben"] += 1
+      turn = 0
+  
+  if roundsWon["Maria"] > roundsWon["Ben"]:
+    return "Maria"
+  elif roundsWon["Maria"] < roundsWon["Ben"]:
+    return "Ben"
+  else:
+    return None
+        
